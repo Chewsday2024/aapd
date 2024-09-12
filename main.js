53610cd87dcf241a9ca7c9318aad7b137d31e5ac
oriascrius <qoq77416416@gmail.com>
2024-09-13 00:57:22 +0800

    更新

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // import Swiper bundle with all modules installed
-// import Swiper from "swiper/bundle";
+import Swiper from "swiper/bundle";
 // import styles bundle
-// import "swiper/css/bundle";
+import "swiper/css/bundle";
 
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
